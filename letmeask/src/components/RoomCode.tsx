7465aa5c4523468670c2679ab285a02561aa1c30
Germano Lira <dolan.3@hotmail.com>
2021-06-23 16:05:56 -0300

    aula 3

---
+++ letmeask/src/components/RoomCode.tsx
@@ -0,0 +1,22 @@
+import copyImg from '../assets/images/copy.svg'
+
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+  code: string,
+} 
+
+export function RoomCode(props: RoomCodeProps) {
+  function codeRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
+  
+  return (
+    <button className="room-code" onClick={codeRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </button>
+  )
+}
